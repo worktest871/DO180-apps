02bdb916c3491c1c146a4a5f8d2ac770e1d5e32b
Sebastian Prechtl <sebastian.prechtl@noris.de>
2021-05-04 11:33:28 +0200

    hah

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Harry Potter is in the house :party:\n";
+print "Harry Potter is in the house\n";
+print "This is a next level comment\n"
 ?>
