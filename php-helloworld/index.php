f78611ef679790774b3ca2db84bf95b7a8e0f0d2
worktest <worktest@871.test>
2021-07-27 14:51:41 +0200

    lel

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -3,5 +3,5 @@ print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "Harry Potter is in the house\n";
 print "This is a next level comment\n";
 print "Thats new\n";
-print "We'll see what will happen";
+print "We'll see what will happen\n";
 ?>
