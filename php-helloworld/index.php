2b669852a3df09311d52e37f3c97f48e0efb33e3
worktest <worktest@871.test>
2021-07-27 14:48:49 +0200

    new change

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -2,6 +2,6 @@
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "Harry Potter is in the house\n";
 print "This is a next level comment\n";
-print "Thats new"\n;
+print "Thats new\n";
 print "We'll see what will happen";
 ?>
